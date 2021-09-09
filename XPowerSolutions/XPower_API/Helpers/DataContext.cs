fe3afe2dfb594a8b602e3eee210afe3b9a227f25
benjatikki <benjamin.tikki@gmail.com>
2021-09-09 11:51:01 +0200

    S1/2/1/ReferenceProject

---
+++ XPowerSolutions/XPower_API/Helpers/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiWithJwtRefreshToken.Entities;
+
+namespace ApiWithJwtRefreshToken.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
