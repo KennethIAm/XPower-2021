67d9569335b8c7a36ad1f28862eafe89374dcbe2
benjatikki <benjamin.tikki@gmail.com>
2021-09-14 14:53:20 +0200

    2/7-FixedTokenCheck

--- XPowerSolutions/XPowerClassLibrary/CommonSettingsFactory.cs
+++ XPowerSolutions/XPowerClassLibrary/CommonSettingsFactory.cs
@@ -12,8 +12,8 @@ namespace XPowerClassLibrary
         internal static SqlConnection GetDBConnectionString(string username, string password)
         {
             // Tempoary hardcoded DB creadentials. Should be removed when specific users are set up.
-            username = "sa";
-            password = "Kode1234!";
+            //username = "sa";
+            //password = "Kode1234!";
             string connectionString = $"Server=172.16.21.33;Database=XPower_Test;User Id={username};Password={password};";
 
             SqlConnection sqlConnection = new SqlConnection(connectionString);
