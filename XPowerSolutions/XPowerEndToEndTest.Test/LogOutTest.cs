af1170d434c5ce563ca0a46f4bfe3bfbf9ed4d89
Jolle <37653945+JohanDraaby@users.noreply.github.com>
2021-09-16 14:37:14 +0200

    s1/3/6-LogOut_ValidUserSession_ShouldLoginAndRedirectToLoginpageThenLogOut
    5 cases for logout

--- XPowerSolutions/XPowerEndToEndTest.Test/LogOutTest.cs
+++ XPowerSolutions/XPowerEndToEndTest.Test/LogOutTest.cs
@@ -4,54 +4,74 @@ using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 
 namespace XPowerEndToEndTest.Test
 {
     public class LogOutTest
     {
-        string testUrl = "http://7351-93-176-82-58.ngrok.io/account/login";
+        string testUrl = "http://2380-93-176-82-58.ngrok.io/";
 
         IWebDriver edgeDriver;
-
         IWebElement emailInput;
-
         IWebElement passwordInput;
-
         IWebElement loginbtn;
-
+        IWebElement burgerMenu;
         IWebElement logOutbtn;
+        IWebElement loginpagetext;
 
         DefaultWait<IWebDriver> wait;
 
         [OneTimeSetUp]
         public void Setup()
         {
-            edgeDriver = new EdgeDriver(@"C:\Users\johan\Desktop\Programming\skole\XPower-2021\XPowerSolutions\XPowerEndToEndTest.Test\bin\Debug");
-
-            wait = new WebDriverWait(edgeDriver, TimeSpan.FromSeconds(10));
         }
 
 
 
-        [Test]
-        public void LogOutSuccessTest()
+        [TestCase("MailTest1@email.com", "PasswordTest")]
+        [TestCase("MailTest6@email.com", "PasswordTest")]
+        [TestCase("MailTest3@email.com", "PasswordTest")]
+        [TestCase("MailTest4@email.com", "PasswordTest")]
+        [TestCase("MailTest5@email.com", "PasswordTest")]
+        public void LogOut_ValidUserSession_ShouldLoginAndRedirectToLoginpageThenLogOut(string mail, string pass)
         {
-            edgeDriver.Url = testUrl;
+            try
+            {
+                edgeDriver = new EdgeDriver(@"C:\Users\johan\Desktop\Programming\skole\XPower-2021\XPowerSolutions\XPowerEndToEndTest.Test\bin\Debug");
+                wait = new WebDriverWait(edgeDriver, TimeSpan.FromSeconds(10));
 
-            emailInput = wait.Until(ExpectedConditions.ElementExists(By.Id("inputEmail")));
-            passwordInput = edgeDriver.FindElement(By.Id("inputPassword"));
-            loginbtn = edgeDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/form/div[3]/button[2]"));
+                edgeDriver.Url = testUrl;
 
-            emailInput.SendKeys("MailTest4@email.com");
-            passwordInput.SendKeys("PasswordTest");
-            loginbtn.Click();
+                emailInput = wait.Until(ExpectedConditions.ElementExists(By.Id("inputEmail")));
+                passwordInput = edgeDriver.FindElement(By.Id("inputPassword"));
+                loginbtn = edgeDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/form/div[3]/button[2]"));
 
-            logOutbtn = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/nav/div/div/span/button")));
-            logOutbtn.Click();
+                emailInput.SendKeys(mail);
+                passwordInput.SendKeys(pass);
+                loginbtn.Click();
 
-            edgeDriver.Close();
-            Assert.Pass();
+                burgerMenu = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/nav/div/button")));
+                burgerMenu.Click();
+                logOutbtn = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/nav/div/div/span/button")));
+                logOutbtn.Click();
+
+                Thread.Sleep(1000);
+                var url = edgeDriver.Url;
+                if (url == "http://2380-93-176-82-58.ngrok.io/account/login")
+                {
+                    Assert.Pass();
+                }
+                else
+                    Assert.Fail();
+
+
+            }
+            finally
+            {
+                edgeDriver.Close();
+            }
         }
 
 
