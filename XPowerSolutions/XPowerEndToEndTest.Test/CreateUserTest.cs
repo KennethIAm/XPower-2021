dd87029984a8c9651d048e82de6b7d3994a1ed12
Jolle <37653945+JohanDraaby@users.noreply.github.com>
2021-09-16 12:52:20 +0200

    s1/3/6-CreateUser-End-To-End-Test
    5 testcases for end to end usercreation with valid info

--- XPowerSolutions/XPowerEndToEndTest.Test/CreateUserTest.cs
+++ XPowerSolutions/XPowerEndToEndTest.Test/CreateUserTest.cs
@@ -5,63 +5,94 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using XPowerClassLibrary.Users;
 
 namespace XPowerEndToEndTest.Test
 {
     public class CreateUserTest
     {
-        string testUrl = "http://7351-93-176-82-58.ngrok.io/account/login";
+        string testUrl = "http://2380-93-176-82-58.ngrok.io/";
 
         IWebDriver edgeDriver;
         IWebElement emailInput;
         IWebElement passwordInput;
-        IWebElement loginbtn;
         IWebElement logOutbtn;
         IWebElement createNavbtn;
         IWebElement username;
         IWebElement reEnterPassword;
         IWebElement Createbtn;
+        IWebElement burgerMenu;
 
         DefaultWait<IWebDriver> wait;
+
 
         [OneTimeSetUp]
         public void Setup()
         {
-            edgeDriver = new EdgeDriver(@"C:\Users\johan\Desktop\Programming\skole\XPower-2021\XPowerSolutions\XPowerEndToEndTest.Test\bin\Debug");
-
-            wait = new WebDriverWait(edgeDriver, TimeSpan.FromSeconds(10));
         }
 
-        [Test]
-        public void TestSuccessCreateUser()
+        [TestCase("Test", "PasswordTest", "Testuser1")]
+        [TestCase("Test", "PasswordTest", "Testuser2")]
+        [TestCase("Test", "PasswordTest", "Testuser3")]
+        [TestCase("Test", "PasswordTest", "Testuser4")]
+        [TestCase("Test", "PasswordTest", "Testuser5")]
+        public void CreateUser_ValidCredentials_ShouldCreateUserAndNavigateToLogin(string mail, string pass, string usernameinput)
         {
-            edgeDriver.Url = testUrl;
+            try
+            {
+                edgeDriver = new EdgeDriver(@"C:\Users\johan\Desktop\Programming\skole\XPower-2021\XPowerSolutions\XPowerEndToEndTest.Test\bin\Debug");
+                wait = new WebDriverWait(edgeDriver, TimeSpan.FromSeconds(10));
 
-            createNavbtn = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/div[1]/div/div/div/div/form/div[3]/button[1]")));
+                edgeDriver.Url = testUrl;
 
-            emailInput = edgeDriver.FindElement(By.Id("inputEmail"));
-            username = edgeDriver.FindElement(By.Id("inputUsername"));
-            passwordInput = edgeDriver.FindElement(By.Id("inputPassword"));
-            reEnterPassword = edgeDriver.FindElement(By.Id("inputReEnterPassword"));
+                createNavbtn = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/div[1]/div/div/div/div/form/div[3]/button[1]")));
+                createNavbtn.Click();
 
-            emailInput.SendKeys("MailTest20@email.com");
-            username.SendKeys("Testacc20");
-            passwordInput.SendKeys("PasswordTest");
-            reEnterPassword.SendKeys("PasswordTest");
-            loginbtn.Click();
+                var rnd = new Random(DateTime.Now.ToString().GetHashCode()).Next(100, 10000).ToString();
+                mail = mail + rnd + "@Test.dk";
 
-            logOutbtn = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/nav/div/div/span/button")));
-            logOutbtn.Click();
+                username = wait.Until(ExpectedConditions.ElementExists(By.Id("inputUsername")));
+                emailInput = edgeDriver.FindElement(By.Id("inputEmail"));
+                passwordInput = edgeDriver.FindElement(By.Id("inputPassword"));
+                reEnterPassword = edgeDriver.FindElement(By.Id("inputReEnterPassword"));
+                Createbtn = edgeDriver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div/form/div[4]/button[2]"));
 
-            edgeDriver.Close();
-            Assert.Pass();
+                emailInput.SendKeys(mail);
+                username.SendKeys(usernameinput);
+                passwordInput.SendKeys(pass);
+                reEnterPassword.SendKeys(pass);
+                Createbtn.Click();
+                //*[@id="inputUsername"]
+                try
+                {
+                    burgerMenu = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/nav/div/button")));
+                    burgerMenu.Click();
+                    logOutbtn = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/nav/div/div/span/button")));
+                    burgerMenu.Click();
+
+                    Assert.Pass();
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        logOutbtn = wait.Until(ExpectedConditions.ElementExists(By.XPath("/html/body/nav/div/div/span/button")));
+                        Assert.Pass();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+
+                edgeDriver.Close();
+                Assert.Pass();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
-
-        [Test]
-        public void TestFaillCreateUser()
-        {
-        }
-
 
 
         [TearDown]
